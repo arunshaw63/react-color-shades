fcac0949b018cdcba28f8e9b28ec8da3ab6087bd
arunshaw <arunshaw63@gmail.com>
2021-06-20 11:41:18 +0530

    Updates

---
+++ precache-manifest.7f75eabc35d535de380e5a6d9a9ae681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad59432f136774561033b8cac2f4420",
+    "url": "/react-color-shades/index.html"
+  },
+  {
+    "revision": "03e68bf26a2b5a9a5b71",
+    "url": "/react-color-shades/static/css/main.df92c19e.chunk.css"
+  },
+  {
+    "revision": "38f37157487bfa7c6ff8",
+    "url": "/react-color-shades/static/js/2.ba2684d3.chunk.js"
+  },
+  {
+    "revision": "a3b754be805875a9fdf6e9e288155397",
+    "url": "/react-color-shades/static/js/2.ba2684d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e68bf26a2b5a9a5b71",
+    "url": "/react-color-shades/static/js/main.3c2589f3.chunk.js"
+  },
+  {
+    "revision": "4f0ea9200a511496b79c",
+    "url": "/react-color-shades/static/js/runtime-main.5e631b42.js"
+  }
+]);
